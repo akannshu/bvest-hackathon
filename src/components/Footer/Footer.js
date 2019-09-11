49978e03e948e903a248bcacf49ac0be4fcb6298
Akanshu <akanshuraj@gmail.com>
2019-09-11 21:56:51 +0530

    added social tags

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,14 +19,14 @@ const Footer = () => {
                         <a href="mailto:kunalagg04@gmail.com">
 
                             <div className="email">
-                                bvest@gmail.com
+                                bvest2019@gmail.com
                             </div>
 
                             </a>
 
                             <div className="icons">
-                            <i class="fab fa-facebook-f fa-lg"></i>
-                            <i class="fab fa-instagram fa-lg"></i>
+                            <a href="https://www.facebook.com/bvest2019/"><i class="fab fa-facebook-f fa-lg"></i></a>
+                            <a href="https://instagram.com/bvest2019?igshid=ig8hlcxnau5b"><i class="fab fa-instagram fa-lg"></i></a>
                             <i class="fab fa-github-alt fa-lg"></i>
 
                             <a href="tel: 8076989248">
@@ -36,7 +36,7 @@ const Footer = () => {
                             </div>
 
                             <div className="details">
-                            <i class="far fa-copyright"></i> Hack@Bvest , Bharati Vidyapeeth College of Engineering
+                            <i class="far fa-copyright"></i> IT Department , Bharati Vidyapeeth College of Engineering
 
                             </div>
 
